4baf050bce6a4d4341913f23da234e3adaa65472
Ana Espejo <bia_espejo@hotmail.com>
2023-05-25 21:04:22 -0300

    feat: install EF, add context

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        //construtor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // mapeamento das entidades
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+    }
+}
