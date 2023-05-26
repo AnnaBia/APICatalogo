c4d68a2433000161a18c666e0c220d9b3e53fd6d
Ana Espejo <bia_espejo@hotmail.com>
2023-05-25 21:34:57 -0300

    feat: register connectionString and context

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// registra a string de conexão presente no appsettings.json
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// define o contexto e o provedor UseMySql
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection,
+    ServerVersion.AutoDetect(mySqlConnection)));
 
 var app = builder.Build();
 
