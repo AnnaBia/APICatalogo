e00e83de21ab6ef32d90c2436b0bb50e33b24a11
Ana Espejo <bia_espejo@hotmail.com>
2023-05-25 21:32:17 -0300

    feat: property Nome in Categoria Model

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -3,6 +3,7 @@
     public class Categoria
     {
         public int CategoriaId { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
     }
 }
