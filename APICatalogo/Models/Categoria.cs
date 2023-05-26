f00d22ebf1fa2ac02b9553ac5cb386dd0527e5d5
Ana Espejo <bia_espejo@hotmail.com>
2023-05-25 21:56:54 -0300

    feat: entity relationship

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,9 +1,18 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models
 {
     public class Categoria
     {
+        // inicia a propriedade Produtos(coleção)
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
         public int CategoriaId { get; set; }
         public string? Nome { get; set; }
         public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; } // propriedade de navegação, que indica um para muitos, ou seja, uma Categoria contém uma coleção de Produtos
     }
 }
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -9,5 +9,8 @@
         public string? ImagemUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
+
+        public int CategoriaId { get; set; } // mapeia a coluna categoriaId - FK
+        public Categoria? Categoria { get; set; } // propriedade de navegação, ou seja, um produto está relacionado a uma categoria
     }
 }
